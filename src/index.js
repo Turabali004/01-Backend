e64325504683889ff138eff4b952bdbcc0cd77e7
TurabAli004 <turabalidev004@gmail.com>
2024-08-03 02:12:36 +0500

    connect database

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: "./env"
+})
+
+
+
+connectDB()
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.error("MongoDB connection error:", error)
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is running on port ${process.env.PORT}...`);
+//         })
+
+//     } catch (error) {
+//         console.log("Error", error)
+//         throw error;
+//     }
+// })()
