7f4439699942d96f4445518f044b95c25de65e62
TurabAli004 <turabalidev004@gmail.com>
2024-08-06 01:21:27 +0500

    Error handling

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = 'Something went wrong',
+        error = [],
+        stack = "",
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.data = null
+        this.success = false
+        this.errors = error
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor) 
+        }
+    }
+}
+
+
+export { apiError };
